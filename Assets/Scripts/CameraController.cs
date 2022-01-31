e08a97f9ef34963d2960a85c187ad2f6b4698393
da-albrecht <albrecht.david3@gmail.com>
2022-02-01 00:18:02 +0100

    added Camera Movement with Cinemachine

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    /*  
+        Cinemachine used for CameraMovement 
+        Third Person Camera > Orbits > Body > Y Damping for changes in Y responsiveness
+        Rig Height and Radius can be changed, play around, so that it feels good
 
+        !! CameraTilt on PlayerInput not Implemented yet!
+
+        Force is added from the CameraPosition and Controller(workd well with the New Camera Movement)
+    */
     public GameObject player;
     Vector3 horizontalInput, verticalInput;
-
+    
     void Update()
     {
         horizontalInput = transform.right * Input.GetAxis("Horizontal");
