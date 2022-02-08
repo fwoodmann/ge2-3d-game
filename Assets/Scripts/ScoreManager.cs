ebecbdbc436ff237bb289a67115d9c0866e1ef85
da-albrecht <56965143+da-albrecht@users.noreply.github.com>
2022-02-08 21:09:30 +0100

    build error fixed

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
